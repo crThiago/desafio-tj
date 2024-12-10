44d39822de65fa322b7ae2ac3c23a57cb8c13a4a
Thiago R Costa <thiago_8777@hotmail.com>
2024-12-09 21:26:17 -0300

    feat(models): Cria todas as Models

---
+++ app/Models/Assunto.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Assunto extends Model
+{
+    protected $table = 'Assunto';
+    public $timestamps = false;
+    protected $primaryKey = 'codAs';
+
+    protected $fillable = ['Descricao'];
+
+    public function livros(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Livro::class,
+            'Livro_Assunto',
+            'Assunto_CodAs',
+            'Livro_Codl',
+        );
+    }
+
+}
