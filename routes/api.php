a7fd03643edc8afbbefbefb14eb760c5de7dea7e
Thiago R Costa <thiago_8777@hotmail.com>
2024-12-10 19:45:28 -0300

    feat(controllers): Cria Controller com teste para autores

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AutorController;
 use App\Http\Controllers\LivroController;
 use Illuminate\Support\Facades\Route;
 
+Route::apiResource('autor', AutorController::class);
 Route::apiResource('livro', LivroController::class);
