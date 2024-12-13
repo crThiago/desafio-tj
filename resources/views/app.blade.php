5c21ba293eeeda417ea13a5b7f6f820feaa65ee8
Thiago R Costa <thiago_8777@hotmail.com>
2024-12-13 11:49:09 -0300

    chore: inclusão de capas de livros + favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- favicon -->
+        <link rel="icon" type="image/x-icon" href="/favicon-livro.ico">
+
         <!-- Scripts -->
         @vite('resources/ts/app.ts')
     </head>
